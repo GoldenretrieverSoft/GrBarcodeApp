fb805d61b964cbeb09c5541ea9b045125212aa19
GoldenretrieverSoft <GoldenretrieverSoft@gmail.com>
2020-08-03 22:44:52 +0900

    src : Add Buttons

--- app/src/main/java/com/grs/grbarcode/MainActivity.kt
+++ app/src/main/java/com/grs/grbarcode/MainActivity.kt
@@ -1,12 +1,116 @@
 package com.grs.grbarcode
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.zxing.ResultPoint
+import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.CaptureManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var captureManager: CaptureManager
+    private var torchState: Boolean = false
+
+    private var scanContinuousState: Boolean = true
+    private lateinit var scanContinuousBG: Drawable
+    lateinit var beepManager: BeepManager
+    private var lastScan = Date()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Active Scan"
+
+        captureManager = CaptureManager(this, barcodeView)
+        captureManager.initializeFromIntent(intent, savedInstanceState)
+//
+        beepManager = BeepManager(this)
+        beepManager.isVibrateEnabled = true
+
+//        scanContinuousBG = actbtn.background//actbtn
+//
+        var callback = object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult?) {
+                result?.let {
+                    val current = Date()
+                    val diff = current.time - lastScan.time
+                    if(diff >= 1000){
+                        textView.text = it.text
+                        lastScan = current
+                        beepManager.playBeepSoundAndVibrate()
+
+                        animateBackground()
+                    }
+                }
+            }
+
+            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) {
+            }
+        }
+
+        actbtn.setOnClickListener(View.OnClickListener {
+            if(!scanContinuousState){
+                scanContinuousState = !scanContinuousState
+//                actbtn.setBackgroundColor(ContextCompat.getColor(InlineScanActivity@this, R.color.colorPrimary))
+                textView.text = "scanning..."
+                barcodeView.decodeContinuous(callback)
+            } else {
+                scanContinuousState = !scanContinuousState
+//                actbtn.background = scanContinuousBG
+                barcodeView.barcodeView.stopDecoding()
+            }
+        })
+        textView.text = "scanning..."
+        barcodeView.decodeContinuous(callback)
+//        btnTorch.setOnClickListener {
+//            if(torchState){
+//                torchState = false
+//                barcodeView.setTorchOff()
+//            } else {
+//                torchState = true
+//                barcodeView.setTorchOn()
+//            }
+//        }
+
     }
+
+    override fun onPause() {
+        super.onPause()
+        captureManager.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        captureManager.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        captureManager.onDestroy()
+    }
+
+    private fun animateBackground(){
+        val colorFrom = resources.getColor(R.color.colorAccent)
+        val colorTo = resources.getColor(R.color.colorPrimary)
+        val colorAnimation =
+            ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo)
+        colorAnimation.duration = 250 // milliseconds
+
+        colorAnimation.addUpdateListener { animator -> actbtn.setBackgroundColor(animator.animatedValue as Int) }
+        colorAnimation.start()
+    }
+
 }
